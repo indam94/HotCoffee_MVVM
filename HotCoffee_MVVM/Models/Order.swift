2b68de3206f9a6a8d44c27bdb071f238245cc466
DONGGUN LEE <d0n99un17@gmail.com>
2019-11-12 18:07:00 -0800

    Implementing Webservice Client & Creating Model

---
+++ HotCoffee_MVVM/Models/Order.swift
@@ -0,0 +1,29 @@
+//
+//  Order.swift
+//  HotCoffee_MVVM
+//
+//  Created by DONGGUN LEE on 11/12/19.
+//  Copyright © 2019 DONGGUN LEE. All rights reserved.
+//
+
+import Foundation
+
+enum CoffeeType: String, Codable{
+    case cappuccino
+    case latte
+    case espressoino
+    case cortado
+}
+
+enum CoffeeSize: String, Codable{
+    case small
+    case medium
+    case large
+}
+
+struct Order{
+    let name: String
+    let email: String
+    let type: CoffeeType
+    let size: CoffeeSize
+}
---
+++ HotCoffee_MVVM/Services/WebService.swift
@@ -0,0 +1,46 @@
+//
+//  WebService.swift
+//  HotCoffee_MVVM
+//
+//  Created by DONGGUN LEE on 11/12/19.
+//  Copyright © 2019 DONGGUN LEE. All rights reserved.
+//
+
+import Foundation
+
+enum NetworkError:Error{
+    case decodingError
+    case domainError
+    case urlError
+}
+
+struct Resource<T: Codable>{
+    let url: URL
+}
+
+class WebService{
+    func load<T>(resource:Resource<T>, completion: @escaping (Result<T, NetworkError>) -> Void){
+        
+        URLSession.shared.dataTask(with: resource.url){
+            data, response, error in
+            
+            guard let data = data, error == nil else{
+                completion(.failure(.domainError))
+                return
+            }
+            
+            let result = try? JSONDecoder().decode(T.self, from: data)
+            if let result = result{
+                DispatchQueue.main.async {
+                    completion(.success(result))
+                }
+            }
+            else{
+                completion(.failure(.decodingError))
+                return
+            }
+            
+        }.resume()
+        
+    }
+}
