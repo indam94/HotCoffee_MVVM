a8ca3850a5babbeec8ec32a35e4925353ac04249
DONGGUN LEE <d0n99un17@gmail.com>
2019-11-12 17:23:16 -0800

    Creating the Add New Order Interface

---
+++ HotCoffee_MVVM/Controllers/OrdersTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  OrdersTableViewController.swift
+//  HotCoffee_MVVM
+//
+//  Created by DONGGUN LEE on 11/12/19.
+//  Copyright © 2019 DONGGUN LEE. All rights reserved.
+//
+
+import UIKit
+
+class OrdersTableViewController: UITableViewController{
+    
+}
