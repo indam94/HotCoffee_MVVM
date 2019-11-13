eee91e019c92fbdc22d382d26553d55b8eb7ee6b
DONGGUN LEE <d0n99un17@gmail.com>
2019-11-12 21:53:35 -0800

    Populating Types of Coffee

--- HotCoffee_MVVM/Controllers/AddOrderViewController.swift
+++ HotCoffee_MVVM/Controllers/AddOrderViewController.swift
@@ -1,5 +1,5 @@
 //
-//  AddOrderNewController.swift
+//  AddOrderViewController.swift
 //  HotCoffee_MVVM
 //
 //  Created by DONGGUN LEE on 11/12/19.
@@ -8,6 +8,43 @@
 
 import UIKit
 
-class AddOrderViewController: UIViewController{
+class AddOrderViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+    
+    private var vm = AddCoffeeOrderViewModel()
+    private var coffeeSizesSegmentCotrol: UISegmentedControl!
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupUI()
+    }
+    
+    private func setupUI(){
+        self.coffeeSizesSegmentCotrol = UISegmentedControl(items: self.vm.sizes)
+        self.coffeeSizesSegmentCotrol.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(self.coffeeSizesSegmentCotrol)
+        
+        self.coffeeSizesSegmentCotrol.topAnchor.constraint(equalTo: self.tableView.bottomAnchor, constant: 20).isActive = true
+        self.coffeeSizesSegmentCotrol.centerXAnchor.constraint(equalTo: self.view.centerXAnchor)
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.vm.types.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CoffeeTypeTableViewCell", for: indexPath)
+        
+        cell.textLabel?.text = self.vm.types[indexPath.row]
+        
+        return cell
+    }
     
 }
--- HotCoffee_MVVM/ViweModels/AddCoffeeOrderViewModel.swift
+++ HotCoffee_MVVM/ViweModels/AddCoffeeOrderViewModel.swift
@@ -7,3 +7,16 @@
 //
 
 import Foundation
+
+struct AddCoffeeOrderViewModel{
+    var name: String?
+    var email: String?
+    
+    var types:[String]{
+        return CoffeeType.allCases.map {$0.rawValue.capitalized}
+    }
+    
+    var sizes:[String]{
+        return CoffeeSize.allCases.map {$0.rawValue.capitalized}
+    }
+}
